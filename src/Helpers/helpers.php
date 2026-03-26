3108935e8c5080c96ed1fcc99f3b5ad22ce26c7c
sajed zarinpour <sa.zarinpour@gmail.com>
2026-03-26 07:06:03 -0700

    playing around with features

--- src/Helpers/helpers.php
+++ src/Helpers/helpers.php
@@ -1,6 +1,22 @@
 <?php
 
 use SajedZarinpour\Meloquent\Meloquent;
+use SajedZarinpour\Meloquent\Models\BaseNonPersistanceModel;
+
+use SajedZarinpour\Meloquent\Attributes\MarkedAsBelongsTo;
+use SajedZarinpour\Meloquent\Attributes\MarkedAsBelongsToMany;
+use SajedZarinpour\Meloquent\Attributes\MarkedAsHasMany;
+use SajedZarinpour\Meloquent\Attributes\MarkedAsHasManyThrough;
+use SajedZarinpour\Meloquent\Attributes\MarkedAsHasOne;
+use SajedZarinpour\Meloquent\Attributes\MarkedAsHasOneOrMany;
+use SajedZarinpour\Meloquent\Attributes\MarkedAsHasOneOrManyThrough;
+use SajedZarinpour\Meloquent\Attributes\MarkedAsHasOneThrough;
+use SajedZarinpour\Meloquent\Attributes\MarkedAsMorphMany;
+use SajedZarinpour\Meloquent\Attributes\MarkedAsMorphOne;
+use SajedZarinpour\Meloquent\Attributes\MarkedAsMorphOneOrMany;
+use SajedZarinpour\Meloquent\Attributes\MarkedAsMorphTo;
+use SajedZarinpour\Meloquent\Attributes\MarkedAsMorphToMany;
+
 /**
  * Adding helper functionality to the package. The following calls are equivalent.
  * ```
@@ -18,90 +34,138 @@ if (!function_exists('meloquent')) {
     }
 }
 
+if(!function_exists('getModelRelationAttributes')){
+    function getModelRelationAttributes(BaseNonPersistanceModel $model){
+        $reflection = new ReflectionObject($model);
+            $methods = $reflection->getMethods(ReflectionMethod::IS_PUBLIC);
+            $list = [];
+            foreach ($methods as $method) {
+                $attributes = $method->getAttributes();
+                if (count($attributes)>0) {
+    
+                    $attrs = array_filter($attributes, static function ($attribute) {
+                        return match ($attribute->name) {
+                            MarkedAsBelongsTo::class,
+                            MarkedAsBelongsToMany::class,
+                            MarkedAsHasMany::class,
+                            MarkedAsHasManyThrough::class,
+                            MarkedAsHasOne::class,
+                            MarkedAsHasOneOrMany::class,
+                            MarkedAsHasOneOrManyThrough::class,
+                            MarkedAsHasOneThrough::class,
+                            MarkedAsMorphMany::class,
+                            MarkedAsMorphOne::class,
+                            MarkedAsMorphOneOrMany::class,
+                            MarkedAsMorphTo::class,
+                            MarkedAsMorphToMany::class=>true,
+                            default => false,
+                        };
+                    });
 
-// function yare()
-// {
-//         $parentModelFactory = function() {
-//             return new class extends Illuminate\Database\Eloquent\Model{
-//                 use SajedZarinpour\Meloquent\Concerns\HasRelation;
-//                 public static int $idGenerator = 1;
-//                 public int $id;
-//                 public string $name;
+                    if (sizeof($attrs)>0) {
+                        $list [] = [
+                            'method' => $method,
+                            'relations'=>$attrs
+                        ];
+                    }
+                    
+                }
+            }
 
-//                 public $c;
+            return $list;
+    }
+}
 
-//                 public function __construct() { 
-//                     $this->id = self::$idGenerator++;
-//                     $this->name = 'parent';
-//                 }
+if(!function_exists('yare')){
+    function yare()
+    {
+        $parentModelFactory = function() {
+            return new class extends SajedZarinpour\Meloquent\Models\BaseNonPersistanceModel{
+                use SajedZarinpour\Meloquent\Concerns\HasRelation;
+                public static int $idGenerator = 1;
 
-//                 public function setChild($c)
-//                 {
-//                     $this->c = $c;
-//                 }
+                public $c;
 
-//                 public function child() {
-//                     return $this->hasMany($this->p::class, 'id');
-//                 }
+                protected $fillable = [
+                    'id',
+                    'name',
+                ];
 
-//                 public function __get($property)
-//                 {
-//                     try {
-//                         return parent::__get($property);
-//                     } catch (\Throwable $th) {
-//                         if ($th instanceof Illuminate\Database\QueryException) {
-//                             return $this->c;
-//                         }
-//                     }  
-//                 }
-//             };
-//         };
+                protected $pKey = [
+                    'field' => 'id',
+                    'type' => 'int',
+                ];
+    
+                public function __construct() 
+                {
+                    $this->_setId();
+                    $this->name = 'parent';
+                }
 
-//         $childModelFactory = function() {
-//             return new class extends Illuminate\Database\Eloquent\Model{
-//                 use SajedZarinpour\Meloquent\Concerns\HasRelation;
-//                 public static int $idGenerator = 1;
-//                 public int $id;
-//                 public int $parent_id;
-//                 public string $name;
-//                 public string $message;
+                public function setChild($c)
+                {
+                    $this->c = $c;
+                }
+    
+                #[MarkedAsHasMany]
+                public function child() 
+                {
+                    // return $this->hasMany($this->c::class, 'id');
+                    return $this->c;
+                }
 
-//                 public $p;
+                private function _setId()
+                {    
+                    $this->id = self::$idGenerator++;
+                }
+            };
+        };
+    
+        $childModelFactory = function() {
+            return new class extends SajedZarinpour\Meloquent\Models\BaseNonPersistanceModel{
+                use SajedZarinpour\Meloquent\Concerns\HasRelation;
+                public static int $idGenerator = 1;
 
-//                 public function __construct() { 
-//                     $this->id = self::$idGenerator++;
-//                 }
+                protected $fillable = [
+                    'id',
+                    'name',
+                    'message',
+                    'parent_id'
+                ];
 
-//                 public function setParent($p)
-//                 {
-//                     $this->parent_id = $p->id;
-//                     $this->p = $p;
-//                 }
+                protected $pKey = [
+                    'field' => 'id',
+                    'type' => 'int',
+                ];
+    
+                public $p;
+    
+                public function __construct() { 
+                    $this->id = self::$idGenerator++;
+                }
+    
+                public function setParent($p)
+                {
+                    $this->parent_id = $p->id;
+                    $this->p = $p;
+                }
+    
+                #[MarkedAsBelongsTo]
+                public function parent() {
+                    // return $this->belongsTo($this->p::class, 'parent_id');
+                    return $this->p;
+                }
+            };
+        };
+    
+        $a = $parentModelFactory();
+        $b = $childModelFactory();
+        $b->message = 'period';
+    
+        $b->setParent($a);
+        $a->setChild($b);
 
-//                 public function parent() {
-//                     return $this->belongsTo($this->p::class, 'parent_id');
-//                 }
+        return $a->message;
+    }
 
-//                 public function __get($property)
-//                 {
-//                     try {
-//                         return parent::__get($property);
-//                     } catch (\Throwable $th) {
-//                         if ($th instanceof Illuminate\Database\QueryException) {
-//                             return $this->p;
-//                         }
-//                     }  
-//                 }
-//             };
-//         };
-
-//         $a = $parentModelFactory();
-//         $b = $childModelFactory();
-//         $b->message = 'period';
-
-//         $b->setParent($a);
-//         $a->setChild($b);
-
-//         var_dump($a->get_('message'));
-
-// }
+}
